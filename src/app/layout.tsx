f1ed16b511a2c642878dfe9e2e8ffe4e5cfd3d85
Eesha Shahid <eesha.shahid14@gmail.com>
2026-08-26 07:14:56 +0500

    feat(1.1): nextjs setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Geist({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -21,9 +22,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}
+        <Toaster />
+      </body>
     </html>
   );
 }
