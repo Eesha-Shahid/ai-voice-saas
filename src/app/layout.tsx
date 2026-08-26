f46b1c7f920a87b36e3b1fb208e795884b500ec9
Eesha Shahid <eesha.shahid14@gmail.com>
2026-08-26 08:00:59 +0500

    feat(1.2): clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Geist({
   variable: "--font-inter",
@@ -20,13 +21,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}
-        <Toaster />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">{children}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button variant="outline" size="xs">
-      Click Me
-    </Button>
-  )
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">Welcome to Sonara</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
+  );
 }
